b493cd5198225735962079e11cecc5b47b70aecd
Hadera <hadera@infinirex.se>
2026-02-06 18:03:08 +0100

    backend_uri

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,31 +1,31 @@
-// // frontend/src/apollo/client.js
-// import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-// import { setContext } from '@apollo/client/link/context';
+// frontend/src/apollo/client.js
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-// // MANUAL FIX: Hardcode production URL
-// const graphqlUri = window.location.hostname.includes('localhost') 
-//   ? 'http://localhost:4000/graphql'  // Local development
-//   : 'https://book-lister-backend-p1ro.vercel.app/graphql'; // Production
+// MANUAL FIX: Hardcode production URL
+const graphqlUri = window.location.hostname.includes('localhost') 
+  ? 'http://localhost:4000/graphql'  // Local development
+  : 'https://book-lister-backend-p1ro.vercel.app/graphql'; // Production
 
-// console.log('🔗 GraphQL URI:', graphqlUri, 'Hostname:', window.location.hostname);
+console.log('🔗 GraphQL URI:', graphqlUri, 'Hostname:', window.location.hostname);
 
-// const httpLink = createHttpLink({
-//   uri: graphqlUri,
-// });
+const httpLink = createHttpLink({
+  uri: graphqlUri,
+});
 
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem('token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
-// export default client;
+export default client;
